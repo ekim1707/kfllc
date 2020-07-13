c2e81ee07bfba3f6c4d54049ac7dc7337b2c6c91
Eugene Kim <ekim1707@gmail.com>
2020-07-13 09:35:57 -0400

    react router bug fix

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Router, Route } from "react-router-dom";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import history from "./history";
 import NavContainer from "./containers/NavContainer";
 import HomeContainer from "./containers/HomeContainer";
